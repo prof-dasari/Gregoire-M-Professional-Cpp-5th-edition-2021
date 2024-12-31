2a5c2fef4536833a2d614fb3daf29fc64f8e9ac3
Dasari <sree91@gmail.com>
2025-01-01 01:37:18 +0530

    commit

--- c07_code/01_ArrayDelete/ArrayDelete.cpp
+++ c07_code/01_ArrayDelete/ArrayDelete.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
-import <iostream>;
+#include <iostream>
+using namespace std;
 
 using namespace std;
 
@@ -12,22 +13,25 @@ public:
 
 int main()
 {
-	Simple* mySimpleArray{ new Simple[4] };
+	Simple *mySimpleArray{new Simple[4]};
 	// Use mySimpleArray...
 	delete[] mySimpleArray;
 	mySimpleArray = nullptr;
 
-
-	const size_t size{ 4 };
-	Simple** mySimplePtrArray{ new Simple*[size] };
+	const size_t size{4};
+	Simple **mySimplePtrArray{new Simple *[size]};
 
 	// Allocate an object for each pointer.
-	for (size_t i{ 0 }; i < size; i++) { mySimplePtrArray[i] = new Simple{}; }
+	for (size_t i{0}; i < size; i++)
+	{
+		mySimplePtrArray[i] = new Simple{};
+	}
 
 	// Use mySimplePtrArray ...
 
 	// Delete each allocated object.
-	for (size_t i{ 0 }; i < size; i++) {
+	for (size_t i{0}; i < size; i++)
+	{
 		delete mySimplePtrArray[i];
 		mySimplePtrArray[i] = nullptr;
 	}
